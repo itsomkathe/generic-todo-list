f22017db01084afa392483b39dad2345fd020edd
Om Kathe <omkathe26@gmail.com>
2021-04-20 23:55:55 +0530

    Updates

---
+++ precache-manifest.1cad65333259dc282297e4553ba593c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc171cc84d2e0a1f1f99caa1b4616c7",
+    "url": "/generic-todo-list/index.html"
+  },
+  {
+    "revision": "3e56d1e0420584a25f51",
+    "url": "/generic-todo-list/static/css/main.9b0d9521.chunk.css"
+  },
+  {
+    "revision": "b4700dec74cf9a7505a0",
+    "url": "/generic-todo-list/static/js/2.d8dcc8c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/generic-todo-list/static/js/2.d8dcc8c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e56d1e0420584a25f51",
+    "url": "/generic-todo-list/static/js/main.728b3009.chunk.js"
+  },
+  {
+    "revision": "fbff39d15799c714bfd3",
+    "url": "/generic-todo-list/static/js/runtime-main.b07d7508.js"
+  }
+]);
